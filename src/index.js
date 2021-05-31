3cd6df50b0d065f6b5180d737d4bfa7f1df9bb6a
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-05-31 14:51:32 +0300

    Доробив

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './sass/main.scss';
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+  
+let colorInterval;
+  
+const startButton = document.querySelector('button[data-action="start"]');
+const stopButton = document.querySelector('button[data-action="stop"]');
+
+startButton.addEventListener("click", startBtnClick);
+stopButton.addEventListener("click", stopBtnClick);
+
+
+function startBtnClick() {
+    startButton.disabled = true;
+    colorInterval = setInterval(setColor,1000);
+};
+
+function stopBtnClick() {
+    startButton.disabled = false;
+    clearInterval(colorInterval);
+};
+
+function setColor() {
+    document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }
+  
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
